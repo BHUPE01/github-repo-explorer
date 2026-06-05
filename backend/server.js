c36811df2d50092f6a37c4a6d4a854aadf87a29d
Bhupendra66 <bhupendrapundir6@email.com>
2026-06-05 20:13:16 +0530

    backend server

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+      res.send('Hello from our server!')
+})
+
+app.use("/:username", getGithubUser);
+
+app.listen(8080, () => {
+      console.log('server listening on port 8080')
+})
